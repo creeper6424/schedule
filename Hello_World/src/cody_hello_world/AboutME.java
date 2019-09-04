b2aabd8b47b98e8f3cfcc532ec49f6b3a2b9041b
Cody Christensen <cody.a.christensen@tooeleschools.org>
2019-09-04 14:11:27 -0600

    class schedule

---
+++ Hello_World/src/cody_hello_world/AboutME.java
@@ -0,0 +1,33 @@
+package cody_hello_world;
+
+public class AboutME {
+
+	public AboutME() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("my name is cody christensen");
+		System.out.println("I go to stansburry high school");
+		System.out.println("I have a verry full schedule");
+		System.out.println("my first pired is history");
+		System.out.println("I chose the middle ages history class");
+		System.out.println("I wanted this one because my only outher option was WWII");
+		System.out.println(" and I have allready studyed that a lot throught middle school");
+		System.out.println("My seckond class is math");
+		System.out.println("I go to calcilous witch is verry hard");
+		System.out.println("tosay I had a test in math");
+		System.out.println("after math I have pottry");
+		System.out.println("I am making a vace in pottery");
+		System.out.println("I want it to end up beeing clear blue ");
+		System.out.println("But I think I might have added too much glase");
+		System.out.println("After pottery I have lunch");
+		System.out.println("For lunch I had a hamburger");
+		System.out.println("after lunch I have english then spanish");
+		System.out.println("It gets confusing after a while having 2 difrest laguenges back to back");
+		System.out.println("The next day my schedule is");
+		System.out.println("PE, band, programing, study hall, and welding");
+		System.out.println("this is my easy day because it has all of my electives");
+	}
+
+}
---
+++ Hello_World/src/cody_hello_world/Heloow_world.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package cody_hello_world;
+
+/**
+ * @author cody.a.christensen
+ *
+ */
+public class Heloow_world {
+
+	/**
+	 * 
+	 */
+	public Heloow_world() {
+			System.out.println("hello world");	
+	}
+	
+	
+	public static void main(String[] args) {
+		new Heloow_world();
+
+	}
+
+}
